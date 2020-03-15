aeb80d8aff989ed3925b4e65dcfdfa6e7fcf7dc9
Dmitry Shkalkov <shkalkov.dima@gmail.com>
2020-03-15 21:23:44 +0300

    feat: implement MiniMaxArrayConverter exercise

--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -4,7 +4,28 @@
 
 // Complete the convertFromArray function below.
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
+
+   
+   NSInteger sum = [[array valueForKeyPath: @"@sum.self"] intValue];
+   
+//   NSNumber *mn = [NSNumber numberWithInt:[[array valueForKeyPath: @"@sum.self"] intValue]];
+//   NSNumber *mx;
+   NSInteger min = sum - [array[0] intValue];
+   NSInteger max = sum - [array[0] intValue];
+   
+   for (int i = 1; i < [array count]; i++) {
+      if (sum - [array[i] intValue] < min) {
+         min = sum - [array[i] intValue];
+      }
+      
+      if (sum - [array[i] intValue] > max) {
+         max = sum - [array[i] intValue];
+      }
+   }
+   
+//   NSLog(@"!!!!! Sum %@", sum);
+   
+   return @[[NSNumber numberWithInt:min], [NSNumber numberWithInt:max]];
 }
 
 @end
