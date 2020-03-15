1e3e7c7609906c39484b94b6c739724f57eade44
Dmitry Shkalkov <shkalkov.dima@gmail.com>
2020-03-15 21:22:45 +0300

    feat: implement T1Array exercise

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,30 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+   if ([sadArray count]) {
+      NSMutableArray *happyArray = [sadArray mutableCopy];
+      
+      BOOL isAllElementsHappy = false;
+
+      while (!isAllElementsHappy) {
+         NSUInteger count = [happyArray count];
+         isAllElementsHappy = true;
+         
+         for (int i = 1; i < count - 1; i++) {
+            BOOL isBadValue = ([happyArray[i - 1] intValue] + [happyArray[i + 1] intValue]) < [happyArray[i] intValue];
+            
+            if (isBadValue) {
+               [happyArray removeObjectAtIndex:i];
+               count--;
+               isAllElementsHappy = false;
+            }
+         }
+      }
+      
+      return happyArray;
+   }
+      
+   return sadArray;
 }
 
 @end
