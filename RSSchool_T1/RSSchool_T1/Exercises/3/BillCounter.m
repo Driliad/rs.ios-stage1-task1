dc2eda41febe42eb4bca543426323f1ee67b390d
Dmitry Shkalkov <shkalkov.dima@gmail.com>
2020-03-15 21:24:14 +0300

    feat: implement BillCounter exercise

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,9 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+   NSInteger totalBillWithoutAnna = [[bill valueForKeyPath: @"@sum.self"] intValue] - [bill[index] intValue];
+   
+   return totalBillWithoutAnna / 2 == [sum intValue] ? @"Bon Appetit" : [NSString stringWithFormat:@"%li", [sum intValue] - totalBillWithoutAnna / 2 ];
 }
 
 @end
